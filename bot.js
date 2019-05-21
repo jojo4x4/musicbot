6c23433b4035c357d94e28da702cf23acb73fe1b
jojo4x4 <jonathan.balraj@gmail.com>
2019-05-21 15:41:47 -0500

    working pinger

--- bot.js
+++ bot.js
@@ -1,39 +1,27 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+const Discord = require('discord.js');
+const config = require('./config.json');
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const bot = new Discord.Client();
+bot.on('ready', () => {
+    console.log('Connected');    
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', msg => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
+    if(!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+	const args = msg.content.slice(config.prefix.length).split(' ');
+    const cmd = args.shift().toLowerCase();
+    
+    switch(cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
+                const then = Date.now();
+                msg.channel.send('Pinging...').then( m => {
+                    m.edit(`Pong! It took ${Date.now() - then}ms to send`);
                 });
             break;
-            // Just add any case commands if you want to..
-         }
-     }
+            default: console.log(`Unhandled case: ${cmd}`)
+         }     
 });
+// Start Discord bot
+bot.login(config.token);
