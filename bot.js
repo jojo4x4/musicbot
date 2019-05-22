30605cd5e11fbc6376fb422d5cf960fa88b4ed3e
jojo4x4 <jonathan.balraj@gmail.com>
2019-05-21 20:34:35 -0500

    use external module for music commands refactor on message to use an object

--- bot.js
+++ bot.js
@@ -7,31 +7,39 @@ client.music = require("discord.js-musicbot-addon");
 client.on('ready', () => {
     console.log('Connected');    
 });
-client.on('message', msg => {
-    console.log(`Message: ${msg}`);
-    // Our client needs to know if it will execute a command
-    // It will listen for messages that will start with `!`    
+client.on('message', msg => {   
     if(!msg.content.startsWith(config.prefix) || msg.author.bot) return;
+
     const args = msg.content.slice(config.prefix.length).split(' ');
     const cmd = args.shift().toLowerCase();
-    
-    switch(cmd) {
-            // !ping
-            case 'ping':
-                const then = Date.now();
-                msg.channel.send('Pinging...').then( m => {
-                    m.edit(`Pong! It took ${Date.now() - then}ms to send`);
-                });
-                break;
-            case 'play':
-                // TODO: handle if suffix empty
-                client.music.bot.playFunction(msg, args[0]); 
-                break;
-            default: console.log(`Unhandled case: ${cmd}`)
-         }     
+    musicCommands = ['musichelp', /*'play'*/, 'search', 'skip', 'queue', 'pause', 'resume',
+        'remove', 'volume', 'leave', 'clearqueue', 'np'];
+
+    if(musicCommands.includes(cmd)){
+        return;
+    }
+         
+    functions = {
+        'ping': (msg, args) => {
+            const then = Date.now();
+            msg.channel.send('Pinging...').then(m => m.edit(`Pong! It took ${Date.now() - then}ms to send`));
+        },
+        'play': (msg, args) => {
+            args.length == 0 ? client.music.bot.resumeFunction(msg) : client.music.bot.playFunction(msg, args[0]);
+        }
+    }
+
+    functions.hasOwnProperty(cmd) ? functions[cmd](msg, args) : msg.channel.send('Error: command not found.');
 });
 // Start Discord client
 client.music.start(client, {
-    youtubeKey: config.youtubeKey
+    youtubeKey: config.youtubeKey,
+    botPrefix: config.prefix,
+    bitRate: "320000",
+    defVolume: 30,
+    bigPicture: true,
+    play: {
+        enabled: false
+    }
   });
 client.login(config.token);
